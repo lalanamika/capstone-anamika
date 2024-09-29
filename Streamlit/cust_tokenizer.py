33bfb5c2d8e7b1411783ea0831ce2e4b3b9fce43
lalanamika <toanamikal@gmail.com>
2024-09-28 23:09:39 -0400

    Change absolute path to relative path in cust_tokenizer to help with Streamlit deployment

--- Streamlit/cust_tokenizer.py
+++ Streamlit/cust_tokenizer.py
@@ -2,13 +2,13 @@ from sklearn.feature_extraction._stop_words import ENGLISH_STOP_WORDS
 
 # Remove units of measurements such as teaspoons, cups, ounces etc. Full list at https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
 # measurements = set(line.strip() for line in open('../data/interim/measurement_list.txt'))
-measurements = set(line.strip() for line in open('C:/Users/anami/Downloads/Data_Science/capstone-anamika/data/interim/measurement_list.txt'))
+measurements = set(line.strip() for line in open('../data/interim/measurement_list.txt'))
 
 # Remove extra adjectives like 'baked', 'thawed', 'cleaned' etc.
-extra_adjectives = set(line.strip() for line in open('C:/Users/anami/Downloads/Data_Science/capstone-anamika/data/interim/extra_adjectives_list.txt'))
+extra_adjectives = set(line.strip() for line in open('../data/interim/extra_adjectives_list.txt'))
 
 # Remove some extra words like 'assorted', 'approximately' etc. QUESTION: Is there a smart way to remove the top 100 such words?
-extra_words = set(line.strip() for line in open('C:/Users/anami/Downloads/Data_Science/capstone-anamika/data/interim/extra_words_list.txt'))
+extra_words = set(line.strip() for line in open('../data/interim/extra_words_list.txt'))
 
 
 """
