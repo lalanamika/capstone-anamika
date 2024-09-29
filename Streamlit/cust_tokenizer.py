350cf1504a4c0128d5119331cfc593356a9aae97
lalanamika <toanamikal@gmail.com>
2024-09-28 23:31:27 -0400

    Attempting to fix path error in streamlit

--- Streamlit/cust_tokenizer.py
+++ Streamlit/cust_tokenizer.py
@@ -4,16 +4,18 @@ from sklearn.feature_extraction._stop_words import ENGLISH_STOP_WORDS
 
 path = os.path.dirname(__file__)
 measurements_file = path+'/measurement_list.txt'
+extra_adjectives_file = path+'/extra_adjectives_list.txt'
+extra_words_list_file = path+'/extra_words_list.txt'
 
 # Remove units of measurements such as teaspoons, cups, ounces etc. Full list at https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
 # measurements = set(line.strip() for line in open('../data/interim/measurement_list.txt'))
 measurements = set(line.strip() for line in open(measurements_file))
 
 # Remove extra adjectives like 'baked', 'thawed', 'cleaned' etc.
-extra_adjectives = set(line.strip() for line in open('extra_adjectives_list.txt'))
+extra_adjectives = set(line.strip() for line in open(extra_adjectives_file))
 
 # Remove some extra words like 'assorted', 'approximately' etc. QUESTION: Is there a smart way to remove the top 100 such words?
-extra_words = set(line.strip() for line in open('extra_words_list.txt'))
+extra_words = set(line.strip() for line in open(extra_words_list_file))
 
 
 """
